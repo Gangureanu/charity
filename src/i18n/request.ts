a86b2618b8a4fa9ba4a4276a98380a874839590a
Stefan <stefan.ggu@protonmail.com>
2026-06-09 14:29:50 +0200

    Fix request locale resolution precedence

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,15 +1,15 @@
 import { getRequestConfig } from "next-intl/server";
 import { routing } from "./routing";
 
-export default getRequestConfig(async ({ requestLocale }) => {
-  let locale = await requestLocale;
+export default getRequestConfig(async ({ requestLocale, locale }) => {
+  let resolvedLocale = locale ?? (await requestLocale);
 
-  if (!locale || !routing.locales.includes(locale as (typeof routing.locales)[number])) {
-    locale = routing.defaultLocale;
+  if (!resolvedLocale || !routing.locales.includes(resolvedLocale as (typeof routing.locales)[number])) {
+    resolvedLocale = routing.defaultLocale;
   }
 
   return {
-    locale,
-    messages: (await import(`../../messages/${locale}.json`)).default,
+    locale: resolvedLocale,
+    messages: (await import(`../../messages/${resolvedLocale}.json`)).default,
   };
 });
